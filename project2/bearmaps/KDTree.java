8189a93890d8cf933422fcfa7f42d0f1501e0b1b
Alexandros Kangkelidis <64306207+alekosomegas@users.noreply.github.com>
2023-09-16 14:09:16 +0300

    KD tree nearest fix

--- project2/bearmaps/KDTree.java
+++ project2/bearmaps/KDTree.java
@@ -13,34 +13,37 @@ public class KDTree implements PointSet {
 		assert points.size() > 0;
 		List<Point> sortedPoints = sortBy('X', points);
 		Point median = getMedian(sortedPoints);
-		this.root = new Node(median);
+		this.root = new Node(median, 'X');
 		buildKDTree(split(sortedPoints, median), root, 'Y');
 	}
 	
 	private class Node {
 		Point point;
 		Node[] children;
-		public Node(Point point) {
+		char axis;
+		public Node(Point point, char axis) {
 			this.point = point;
+			this.axis = axis;
 			children = new Node[2];
 		}
 	}
 	private List<Point> sortBy(char by, List<Point> points) {
 		List<Point> copy = points.stream().collect(Collectors.toList());
 		if (by == 'X') {
-			copy.sort(Comparator.comparingDouble(Point::getX));
+			points.sort(Comparator.comparingDouble(Point::getX));
 		} else {
-			copy.sort(Comparator.comparingDouble(Point::getY));
+			points.sort(Comparator.comparingDouble(Point::getY));
 		}
 		return copy;
 	}
 	
 	private Point getMedian(List<Point> points) {
-		return points.get((int) Math.floor(points.size()/2));
+		if (points.size() == 1) return points.get(0);
+		return points.get((int) Math.floor(points.size()/2) -1) ;
 	}
 	
 	private List<Point>[] split(List<Point> square, Point median) {
-		List<Point>[] subSquares = new List<>[2];
+		List<Point>[] subSquares = new List[2];
 		subSquares[0] = square.subList(0, square.indexOf(median));
 		subSquares[1] = square.subList(square.indexOf(median)+1, square.size());
 		return subSquares;
@@ -49,9 +52,10 @@ public class KDTree implements PointSet {
 	private void buildKDTree(List<Point>[] squares, Node current, char sort) {
 		if (squares[0].isEmpty() && squares[1].isEmpty()) return;
 		for (int i = 0; i < 2; i++) {
+			if (squares[i].isEmpty()) continue;
 			List<Point> currSquare = sortBy(sort, squares[i]);
 			Point median = getMedian(currSquare);
-			current.children[i] = new Node(median);
+			current.children[i] = new Node(median, sort);
 			List<Point>[] subSquares = split(squares[i], median);
 			char nextSort = sort == 'X' ? 'Y' : 'X';
 			buildKDTree(subSquares, current.children[i], nextSort);
@@ -60,6 +64,49 @@ public class KDTree implements PointSet {
 	
 	@Override
 	public Point nearest(double x, double y) {
-		return null;
+		Point goal = new Point(x,y);
+		Node best = nearest(root, goal, root);
+		return best.point;
+	}
+
+	// index 0 is good, 1 is bad
+	private Node[] getSides(Node current, Point goal) {
+		char axis = current.axis;
+		int good, bad;
+		Node[] sides = new Node[2];
+		if (axis == 'X') {
+			good = goal.getX() <= current.point.getX() ? 0 : 1;
+		} else {
+			good = goal.getY() <= current.point.getY() ? 0 : 1;
+		}
+		bad = good == 1 ? 0 : 1;
+		sides[0] = current.children[good];
+		sides[1] = current.children[bad];
+		return sides;
+	}
+	
+	private boolean isWorthCheckingBadSide(Node current, Point goal, double bestDistance) {
+		char axis = current.axis;
+		if (axis == 'X') {
+			return Math.pow(Math.abs(goal.getX() - current.point.getX()),2) <= bestDistance ;
+		} else {
+			return Math.pow(Math.abs(goal.getY() - current.point.getY()),2) <= bestDistance;
+		}
+	}
+	private Node nearest(Node current, Point goal, Node best) {
+		if (current == null) return best;
+		double best_distance = Point.distance(best.point, goal);
+		if (Point.distance(current.point, goal) < best_distance) {
+			best = current;
+			best_distance = Point.distance(current.point, goal);
+		}
+		Node[] sides = getSides(current, goal);
+		Node goodSide = sides[0];
+		best = nearest(goodSide, goal, best);
+		if (isWorthCheckingBadSide(current, goal, best_distance)) {
+			Node badSide = sides[1];
+			best = nearest(badSide, goal, best);
+		}
+		return best;
 	}
 }
--- project2/bearmaps/KDTreeTest.java
+++ project2/bearmaps/KDTreeTest.java
@@ -1,7 +1,131 @@
 package bearmaps;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class KDTreeTest {
+	private static final Random r = new Random(500);
+	@Test
+	public void testNaive() {
+		Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+		Point p2 = new Point(3.3, 4.4);
+		Point p3 = new Point(-2.9, 4.2);
+		
+		NaivePointSet nn = new NaivePointSet(List.of(p1, p2, p3));
+		Point ret = nn.nearest(3.0, 4.0); // returns p2
+		assertEquals(p2, ret);
+		assertEquals(p2.getX(), ret.getX(), 0);
+	}
+	
+	@Test
+	public void testBuild() {
+		Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+		Point p2 = new Point(3.3, 4.4);
+		Point p3 = new Point(-2.9, 4.2);
+		
+		KDTree nn = new KDTree(List.of(p1, p2, p3));
+		Point ret = nn.nearest(3.0, 4.0); // returns p2
+		assertEquals(p2, ret);
+		assertEquals(p2.getX(), ret.getX(), 0);
+	}
+	
+	@Test
+	public void simpleTest() {
+		Point a = new Point(2,3);
+		Point b = new Point(4,2);
+		Point c = new Point(4,5);
+		Point d = new Point(3,3);
+		Point e = new Point(1,5);
+		
+		KDTree kdTree = new KDTree(List.of(a,b,c,d,e));
+		Point n = kdTree.nearest(0,7);
+		assertEquals(e, n);
+	}
+	
+	@Test
+	public void bigTest()  {
+		Random random = new Random();
+		List<Point> points = new ArrayList<>();
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000000; i++) {
+			Point p = new Point(random.nextInt(), random.nextInt());
+			points.add(p);
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("Total time elapsed loop: " + (end - start)/1000.0 +  " seconds.");
+		
+		start = System.currentTimeMillis();
+		NaivePointSet naivePointSet = new NaivePointSet(points);
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed Naive init: " + (end - start)/1000.0 +  " seconds.");
+		
+		start = System.currentTimeMillis();
+		KDTree kdTree = new KDTree(points);
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed KD init: " + (end - start)/1000.0 +  " seconds.");
+		
+		Point target = new Point(random.nextInt(), random.nextInt());
+		
+		start = System.currentTimeMillis();
+		Point kn = kdTree.nearest(target.getX(), target.getY());
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed KD: " + (end - start)/1000.0 +  " seconds.");
+		
+		start = System.currentTimeMillis();
+		Point nn = naivePointSet.nearest(target.getX(), target.getY());
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed N: " + (end - start)/1000.0 +  " seconds.");
+		
+		assertEquals(kn,nn);
+	}
+	
+	@Test
+	public void bigRandTest()  {
+		List<Point> points = new ArrayList<>();
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 100000; i++) {
+			Point p = new Point(r.nextDouble(), r.nextDouble());
+			points.add(p);
+		}
+		
+		long end = System.currentTimeMillis();
+		System.out.println("Total time elapsed loop: " + (end - start)/1000.0 +  " seconds.");
+		
+		start = System.currentTimeMillis();
+		NaivePointSet naivePointSet = new NaivePointSet(points);
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed Naive init: " + (end - start)/1000.0 +  " seconds.");
+		
+		start = System.currentTimeMillis();
+		KDTree kdTree = new KDTree(points);
+		end = System.currentTimeMillis();
+		System.out.println("Total time elapsed KD init: " + (end - start)/1000.0 +  " seconds.");
+		
+		double kdTotal = 0.0;
+		double nTotal = 0.0;
+		for (int i = 0; i< 10000; i++) {
+			Point target = new Point(r.nextDouble(), r.nextDouble());
+			
+			start = System.currentTimeMillis();
+			Point kn = kdTree.nearest(target.getX(), target.getY());
+			end = System.currentTimeMillis();
+			kdTotal += (end - start)/1000.0;
+			
+			start = System.currentTimeMillis();
+			Point nn = naivePointSet.nearest(target.getX(), target.getY());
+			end = System.currentTimeMillis();
+			nTotal += (end - start)/1000.0;
+			
+			assertEquals(nn.getX(),kn.getX(), 0.00001);
+			assertEquals(nn.getY(),kn.getY(), 0.00001);
+		}
+		System.out.println("Total time elapsed KD: " + kdTotal +  " seconds.");
+		System.out.println("Total time elapsed N: " + nTotal +  " seconds.");
+		
+	}
 }
